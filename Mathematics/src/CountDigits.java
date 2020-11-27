829ac9bcc0f85c76058c901c2e08f8803b8327bc
Suraj Singh <surajsingh220698@outlook.com>
2020-11-27 13:36:53 +0530

    Rename the CountDigit Class

--- Mathematics/src/CountDigits.java
+++ Mathematics/src/CountDigits.java
@@ -1,5 +1,5 @@
 // A program to find out the number of digits in a number.
-public class countDigits {
+public class CountDigits {
     public static void main(String[] args) {
         System.out.println(count(192810));
     }
