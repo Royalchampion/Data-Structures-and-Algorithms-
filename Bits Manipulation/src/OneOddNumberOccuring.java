3b1a7433694e13be9741613d332d9b7c34eb6209
Suraj Singh <surajsingh220698@outlook.com>
2020-12-16 17:46:05 +0530

    Add the Odd number occuring Program

---
+++ Bits Manipulation/src/OneOddNumberOccuring.java
@@ -0,0 +1,14 @@
+//  A program to findout the odd number of an element in an array.
+public class OneOddNumberOccuring {
+    public static void main(String[] args) {
+        System.out.println(findOdd(new int[]{4, 3, 3, 4, 4, 4, 4}));
+    }
+
+    public static int findOdd(int arr[]) {
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            result = result ^ arr[i];
+        }
+        return result;
+    }
+}
