c662b8b796f2b5f3d49e5be42d947f46fb0b79f7
Suraj Singh <surajsingh220698@outlook.com>
2021-01-01 17:38:11 +0530

    Add to to count number of bits to

---
+++ Bits Manipulation/src/CountBitFlip.java
@@ -0,0 +1,15 @@
+public class CountBitFlip {
+    public static void main(String[] args) {
+        System.out.println(countBitsFlip(10, 20));
+    }
+
+    public static int countBitsFlip(int a, int b){
+        int count = 0;
+        int result = a ^ b;
+        while(result>0) {
+            result = (result&(result-1));
+            count++;
+        }
+        return count;
+    }
+}
