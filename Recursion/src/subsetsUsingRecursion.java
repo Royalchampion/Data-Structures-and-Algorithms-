6ceb18faef0f96a22f17ed9fcc603158c89730c7
Suraj Singh <surajsingh220698@outlook.com>
2021-01-04 18:56:27 +0530

    Add to find the subsets of the given string using program.

---
+++ Recursion/src/subsetsUsingRecursion.java
@@ -0,0 +1,15 @@
+public class subsetsUsingRecursion {
+    public static void main(String[] args) {
+        subsets("ABC", "", 0);
+    }
+
+    public static void subsets(String inputString, String currentString, int i) {
+        if(i==inputString.length()) {
+            System.out.println(currentString + "");
+            return;
+        }
+
+        subsets(inputString, currentString, i+1);
+        subsets(inputString, currentString+inputString.charAt(i), i+1);
+    }
+}
