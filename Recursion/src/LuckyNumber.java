da792d09315baf310e9c0d63601017c74ffe29fc
Suraj Singh <surajsingh220698@outlook.com>
2021-01-07 16:51:41 +0530

    Add to figure out whether the given number is lucky or not program.

---
+++ Recursion/src/LuckyNumber.java
@@ -0,0 +1,19 @@
+public class LuckyNumber {
+    static int counter=2;
+    public static void main(String[] args) {
+        System.out.println(isLucky(19));
+    }
+
+    public static boolean isLucky(int n) {
+        if(n<counter) {
+            return true;
+        }
+        else if(n%counter==0) {
+            return false;
+        }
+
+        n=n-n/counter;
+        counter++;
+        return isLucky(n);
+    }
+}
