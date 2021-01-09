55cb19c1226976ddfe23e44f3b29b5d6422c2011
Suraj Singh <surajsingh220698@outlook.com>
2021-01-09 13:12:07 +0530

    Add to figure out whether the following input array is sorted or not efficient solution program.

--- 4. Array/src/CheckIfAnArrayIsSorted.java
+++ 4. Array/src/CheckIfAnArrayIsSorted.java
@@ -1,14 +1,27 @@
 public class CheckIfAnArrayIsSorted {
     public static void main(String[] args) {
-        System.out.println(isSorted(new int[]{3, 6, 12, 21}));
+        System.out.println(isSortedNaive(new int[]{3, 6, 12, 21}));
+        System.out.println(isSortedEfficient(new int[]{3, 4, 17, 44, 22}));
     }
 
-    public static boolean isSorted(int arr[]) {
+    public static boolean isSortedNaive(int arr[]) {
         for (int i=0; i<arr.length; i++) {
             for (int j=i+1; j<arr.length; j++) {
                 if (arr[i]>arr[j]) {
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isSortedEfficient(int arr[]) {
+        for (int i=0; i<arr.length; i++) {
+            if (i==arr.length-1) {
+                break;
+            }
+            if (arr[i]>arr[i+1]) {
+                return false;
             }
         }
         return true;
