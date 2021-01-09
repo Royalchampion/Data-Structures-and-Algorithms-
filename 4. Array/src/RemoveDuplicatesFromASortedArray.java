f6909afdbf841dbccd32c53adbb8d1ce141d9727
Suraj Singh <surajsingh220698@outlook.com>
2021-01-09 19:13:12 +0530

    Add to remove the duplicate element in an array efficient solution program.

--- 4. Array/src/RemoveDuplicatesFromASortedArray.java
+++ 4. Array/src/RemoveDuplicatesFromASortedArray.java
@@ -1,9 +1,30 @@
 public class RemoveDuplicatesFromASortedArray {
     public static void main(String[] args) {
-        System.out.println(removeDuplicates(new int[]{3, 4, 4, 5, 5, 5}));
+        int arr[] = new int[]{3, 4, 4, 5, 5, 5};
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        int n = removeDuplicatesNaive(arr);
+        for (int i=0; i<n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        int arr1[] = new int[]{3, 12, 34, 22, 22};
+        for (int i=0; i<arr1.length; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+        System.out.println();
+        n = removeDuplicatesEfficient(arr1);
+        for (int i=0; i<n; i++) {
+            System.out.print(arr1[i] + " ");
+        }
     }
 
-    public static int removeDuplicates(int arr[]) {
+//    Time Complexity : O(n)
+//    Space Complexity : O(1)
+    public static int removeDuplicatesNaive(int arr[]) {
         int n = arr.length;
         int temp[] = new int[n];
         temp[0] = arr[0];
@@ -16,6 +37,20 @@ public class RemoveDuplicatesFromASortedArray {
         }
         for (int j=0; j<res; j++) {
             arr[j]=temp[j];
+        }
+        return res;
+    }
+
+//    Time Complexity : O(n)
+//    Space Complexity : O(1)
+//    Since, the above method consumes the array space very much unlike this method, Hence this method is efficient.
+    public static int removeDuplicatesEfficient(int arr[]) {
+        int res=1;
+        for (int i=1; i<arr.length; i++) {
+            if (arr[res-1]!=arr[i]) {
+                arr[res]=arr[i];
+                res++;
+            }
         }
         return res;
     }
