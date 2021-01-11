7cf01ecd8a11501509223cb32ead98ae5f73c86a
Suraj Singh <surajsingh220698@outlook.com>
2021-01-11 14:21:10 +0530

    Add Rotate the array to the left by D position better solution program.

--- 4. Array/src/LeftRotateAnArrayByDPosition.java
+++ 4. Array/src/LeftRotateAnArrayByDPosition.java
@@ -2,12 +2,31 @@ import java.util.Arrays;
 
 public class LeftRotateAnArrayByDPosition {
     public static void main(String[] args) {
-        leftRotate(new int[]{4, 1, 2, 8, 10}, 3);
+        leftRotateNaive(new int[]{4, 1, 2, 8, 10}, 3);
+        leftRotateBetter(new int[]{3, 1, 22, 44, 11}, 4);
     }
 
+////////////////////////////////////////////////////////////////////////////////////////
+//    Time Complexity : O(n);
+//    Auxiliary Space : O(D)
+    public static void leftRotateBetter(int arr[], int D) {
+        int temp[] = new int[D];
+        for (int i=0; i<D; i++) {
+            temp[i] = arr[i];
+        }
+        for (int i=D; i<arr.length; i++) {
+            arr[i-D] = arr[i];
+        }
+        for (int i=0; i<D; i++) {
+            arr[arr.length-D+i] = temp[i];
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////
 //    Time Complexity : O(nD)
 //    Auxiliary Space : O(1)
-    public static void leftRotate(int arr[], int D) {
+    public static void leftRotateNaive(int arr[], int D) {
         for (int i=1; i<=D; i++) {
             int temp = arr[0];
             for (int j=1; j<arr.length; j++) {
