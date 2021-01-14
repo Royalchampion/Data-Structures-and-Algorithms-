e5ebd6e0cd07b3715c91331d100e62eb9a3868bf
Suraj Singh <surajsingh220698@outlook.com>
2021-01-14 22:05:44 +0530

    Add Window Sliding Technique solution program.

---
+++ 4. Array/src/WindowSlidingTechnique.java
@@ -0,0 +1,20 @@
+public class WindowSlidingTechnique {
+    public static void main(String[] args) {
+        System.out.println(findTheMaxSum(new int[]{1, 8, 30, -5, 20, 7}, 3));
+    }
+
+//    Time Complexity : O((n-k)*(n+k-1))
+//    Auxiliary Space : O(1)
+    public static int findTheMaxSum(int arr[], int k) {
+        int res=0;
+        int sum=0;
+        for (int i=0; i<=arr.length-k; i++) {
+            for (int j=i; j<=i+k-1; j++) {
+                sum += arr[j];
+            }
+            res = Math.max(res, sum);
+            sum=0;
+        }
+        return res;
+    }
+}
