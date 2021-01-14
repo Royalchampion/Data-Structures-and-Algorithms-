a7e013a1262bdebdb1a3bb232714146de10703dc
Suraj Singh <surajsingh220698@outlook.com>
2021-01-14 17:19:03 +0530

    Add find the maximum subarray efficient solution program.

--- 4. Array/src/MaximumSubarraySum.java
+++ 4. Array/src/MaximumSubarraySum.java
@@ -1,6 +1,7 @@
 public class MaximumSubarraySum {
     public static void main(String[] args) {
         System.out.println(maxSum(new int[]{1, -2, 3, -1, 2}));
+        System.out.println(maxSumKadaneAlgo(new int[]{-3, 8, -2, 4, -5, 6}));
     }
 
 //    Time Complexity : O(n^2)
@@ -13,6 +14,18 @@ public class MaximumSubarraySum {
                 curr = curr + arr[j];
                 res = Math.max(res, curr);
             }
+        }
+        return res;
+    }
+
+//    Time Complexity : O(n)
+//    Auxiliary Space : O(1)
+    public static int maxSumKadaneAlgo(int arr[]) {
+        int res = arr[0];
+        int maxEnding = arr[0];
+        for (int i=1; i<arr.length; i++) {
+            maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+            res = Math.max(maxEnding, res);
         }
         return res;
     }
