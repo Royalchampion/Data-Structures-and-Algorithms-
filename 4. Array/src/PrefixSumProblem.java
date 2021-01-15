bc58361f0f17451589bbec9aec87b8be96fd7aef
Suraj Singh <surajsingh220698@outlook.com>
2021-01-15 22:53:22 +0530

    Add Prefix Sum Array modified alternative solution program.

--- 4. Array/src/PrefixSumProblem.java
+++ 4. Array/src/PrefixSumProblem.java
@@ -3,18 +3,18 @@ import java.util.Arrays;
 public class PrefixSumProblem {
     public static void main(String[] args) {
         System.out.println(calcSum(new int[]{2, 8, 3, 9, 6, 5, 4}, 1, 3));
-        System.out.println(prefixSumArray(new int[]{2, 8, 3, 9, 6, 5, 4}));
+        System.out.println(prefixSumArray(new int[]{2, 8, 3, 9, 6, 5, 4}, 1, 3));
     }
 
 //    Now, In order to find out the prefix sum on a specific index position, You have to use calcSum method.
 //    Just use the formula : prefixSum(left) - prefixSum(right-1)
-    public static String prefixSumArray(int arr[]) {
+    public static int prefixSumArray(int arr[], int left, int right) {
         int prefixSum[] = new int[arr.length];
         prefixSum[0] = arr[0];
         for (int i=1; i<arr.length; i++) {
             prefixSum[i] = prefixSum[i-1] + arr[i];
         }
-        return Arrays.toString(prefixSum);
+        return (prefixSum[right] - prefixSum[left-1]);
     }
 
 //    Time Complexity : O(n)
