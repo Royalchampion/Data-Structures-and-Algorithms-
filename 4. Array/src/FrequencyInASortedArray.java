85399a7ebd7ad3090c66faf855a981e1827320fe
Suraj Singh <surajsingh220698@outlook.com>
2021-01-17 00:05:49 +0530

    Modify the Frequency in a sorted array program.

--- 4. Array/src/FrequencyInASortedArray.java
+++ 4. Array/src/FrequencyInASortedArray.java
@@ -1,8 +1,11 @@
+// Write a program to figure out the frequency of an element in a sorted array.
 public class FrequencyInASortedArray {
     public static void main(String[] args) {
-        printFrequencyInASortedArray(new int[]{10, 10, 10, 22, 22, 44, 88, 88});
+        printFrequencyInASortedArray(new int[]{88});
     }
 
+//    Time Complexity : O(n)
+//    Space Complexity : O(1)
     public static void printFrequencyInASortedArray(int arr[]) {
         int freq = 1;
         int i = 1;
@@ -14,6 +17,9 @@ public class FrequencyInASortedArray {
             System.out.println(arr[i-1] + " : " + freq);
             i++;
             freq = 1;
+        }
+        if (arr.length==1 || arr[arr.length-1]!=arr[arr.length-2]) {
+            System.out.println(arr[arr.length-1] + " : " + 1);
         }
     }
 }
