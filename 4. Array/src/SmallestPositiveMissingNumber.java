7009bc47a8ed380f67113456bc1ea70e639e63a1
Suraj Singh <surajsingh220698@outlook.com>
2021-01-19 22:12:19 +0530

    Add find the smallest positive missing number program.

---
+++ 4. Array/src/SmallestPositiveMissingNumber.java
@@ -0,0 +1,24 @@
+public class SmallestPositiveMissingNumber {
+//    Time Complexity : O(n)
+//    Auxiliary Space : O(1)
+    static int missingNumber(int arr[], int size)
+    {
+        int n = arr.length;
+
+        boolean[] present = new boolean[n+1];
+
+        for(int i=0; i<n; i++) {
+            if(arr[i] > 0 && arr[i] <= n) {
+                present[arr[i]] = true;
+            }
+        }
+
+        for(int i=1; i<=n; i++) {
+            if(!present[i]) {
+                return i;
+            }
+        }
+
+        return n+1;
+    }
+}
