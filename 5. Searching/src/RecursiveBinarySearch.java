e521b69178841d019f45eba7443a32eba342764d
Suraj Singh <surajsingh220698@outlook.com>
2021-01-21 13:58:19 +0530

    Add the time complexity and auxiliary space for the recursive binary search.

--- 5. Searching/src/RecursiveBinarySearch.java
+++ 5. Searching/src/RecursiveBinarySearch.java
@@ -3,6 +3,8 @@ public class RecursiveBinarySearch {
         System.out.println(recursiveBinarySearch(new int[]{10, 20, 30, 40, 50}, 0, 4, 10));
     }
 
+//     Time Complexity : O(log(n))
+//     Auxiliary Space : O(log(n))
     public static int recursiveBinarySearch(int arr[], int low, int high, int number) {
         if (low>high) {
             return -1;
