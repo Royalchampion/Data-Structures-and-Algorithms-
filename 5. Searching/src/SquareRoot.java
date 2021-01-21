ce5962c42256ac8263d9c8ff662275d060912c36
Suraj Singh <surajsingh220698@outlook.com>
2021-01-21 21:47:44 +0530

    Add find the square root of the given number efficient solution program.

--- 5. Searching/src/SquareRoot.java
+++ 5. Searching/src/SquareRoot.java
@@ -1,6 +1,30 @@
 public class SquareRoot {
     public static void main(String[] args) {
-        System.out.println(findSquareRootNaive(37));
+//        System.out.println(findSquareRootNaive(37));
+        System.out.println(findSquareRootEfficient(26));
+    }
+
+//    Time Complexity : O(log(n))
+//    Auxiliary Space : O(1)
+    public static int findSquareRootEfficient(int number) {
+        int low = 1;
+        int high = number;
+        int result = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int sqr = mid*mid;
+            if (sqr == number) {
+                return mid;
+            }
+            else if (sqr > number) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+                result = mid;
+            }
+        }
+        return result;
     }
 
 //    Time Complexity : O(sqrt(n))
