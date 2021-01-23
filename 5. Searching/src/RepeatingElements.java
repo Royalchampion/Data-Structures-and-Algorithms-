99e3778b388253d93c4dfc8c54d2bab2fb6b5319
Suraj Singh <surajsingh220698@outlook.com>
2021-01-23 21:10:10 +0530

    Add Find the repeating element another efficient solution program.

--- 5. Searching/src/RepeatingElements.java
+++ 5. Searching/src/RepeatingElements.java
@@ -1,6 +1,9 @@
+import java.util.Arrays;
+
 public class RepeatingElements {
     public static void main(String[] args) {
         System.out.println(findTheRepeatingElementNaive(new int[]{0, 2, 1, 3, 2, 2}));
+        System.out.println(findTheRepeatingElementEfficient(new int[]{0, 2, 1, 3, 2, 2}));
     }
 
 //    Time Complexity : O(n*n)
@@ -12,6 +15,20 @@ public class RepeatingElements {
                     return arr[i];
                 }
             }
+        }
+        return -1;
+    }
+
+//    Time Complexity : O(n)
+//    Auxiliary Space : O(n)
+    public static int findTheRepeatingElementEfficient(int arr[]) {
+        boolean visited[] = new boolean[arr.length - 1];
+        Arrays.fill(visited, Boolean.FALSE);
+        for (int i=0; i<arr.length; i++) {
+            if (visited[arr[i]]) {
+                return arr[i];
+            }
+            visited[arr[i]] = true;
         }
         return -1;
     }
