ff0d00c6faa660dbc62b726841f99dea7a763c74
Suraj Singh <surajsingh220698@outlook.com>
2021-01-30 16:58:17 +0530

    Add Find the maximum difference between two buildings efficient solution program.

--- 5. Searching/src/MaximumWaterBetweenTwoBuildings.java
+++ 5. Searching/src/MaximumWaterBetweenTwoBuildings.java
@@ -1,7 +1,8 @@
 public class MaximumWaterBetweenTwoBuildings {
+
 //    Time Complexity : O(n*n)
 //    Auxiliary Space : O(1)
-    public static int findMaxCapacity(int arr[]) {
+    public static int findMaxCapacityNaive(int arr[]) {
         int result = 0;
         for (int i=0; i<arr.length; i++) {
             for (int j=i+1; j<arr.length; j++) {
@@ -10,5 +11,30 @@ public class MaximumWaterBetweenTwoBuildings {
             }
         }
         return result;
+    }
+
+//    Time Complexity : O(n)
+//    Auxiliary Space : O(1)
+    public static int findMaxCapacityEfficient(int height[]) {
+        int i = 0;
+        int j = height.length-1;
+        int max = 0;
+
+        while(i<j) {
+            if(height[i] < height[j]) {
+                max = Math.max(max, height[i]*(j-i-1));
+                i++;
+            }
+            else if(height[j] < height[i]) {
+                max = Math.max(max, height[j]*(j-i-1));
+                j--;
+            }
+            else {
+                max = Math.max(max, height[j]*(j-i-1));
+                i++;
+                j--;
+            }
+        }
+        return max;
     }
 }
