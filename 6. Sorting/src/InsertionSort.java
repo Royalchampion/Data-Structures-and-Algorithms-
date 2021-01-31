b4ff51b9a5c8f7e56d932df71033171c7285bfc2
Suraj Singh <surajsingh220698@outlook.com>
2021-01-31 21:20:59 +0530

    Add the insertion sort algorithm naive solution program.

---
+++ 6. Sorting/src/InsertionSort.java
@@ -0,0 +1,15 @@
+public class InsertionSort {
+//    Time Complexity : O(n*n)
+//    Auxiliary Space : O(1)
+    public static void insertionSort(int arr[]) {
+        for (int i=1; i<arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j>=0 && arr[j]>key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+}
