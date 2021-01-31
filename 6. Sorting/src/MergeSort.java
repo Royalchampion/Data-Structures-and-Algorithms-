0e258a75dcf060cc65ae65d8c515de16de3cce5a
Suraj Singh <surajsingh220698@outlook.com>
2021-01-31 22:14:02 +0530

    Add the merge sort algorithm efficient solution program.

--- 6. Sorting/src/MergeSort.java
+++ 6. Sorting/src/MergeSort.java
@@ -4,12 +4,13 @@ public class MergeSort {
     public static void main(String[] args) {
         int a[] = {10, 15, 20, 20};
         int b[] = {1, 12};
-        mergeSort(a, b);
+        mergeSortNaive(a, b);
+        mergeSortEfficient(a, b);
     }
 
 //    Time Complexity : O((m+n)log(m+n))
 //    Auxiliary Space : O(m+n)
-    public static void mergeSort(int a[], int b[]) {
+    public static void mergeSortNaive(int a[], int b[]) {
         int m = a.length;
         int n = b.length;
         int c[] = new int[m+n];
@@ -21,5 +22,32 @@ public class MergeSort {
         }
         Arrays.sort(c);
         System.out.println(Arrays.toString(c));
+    }
+
+//    Time Complexity : O(m+n)
+//    Auxiliary Space : O(1)
+    public static void mergeSortEfficient(int a[], int b[]) {
+        int i = 0;
+        int j = 0;
+        int m = a.length;
+        int n = b.length;
+        while (i<m && j<n) {
+            if ((a[i] <= b[j])) {
+                System.out.print(a[i] + " ");
+                i++;
+            }
+            else {
+                System.out.print(b[j] + " ");
+                j++;
+            }
+        }
+        while (i<m) {
+            System.out.print(a[i] + " ");
+            i++;
+        }
+        while (j<n) {
+            System.out.print(b[j] + " ");
+            j++;
+        }
     }
 }
