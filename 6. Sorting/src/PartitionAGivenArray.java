430693225a56b5d372c604976fae739adccc2e4b
Suraj Singh <surajsingh220698@outlook.com>
2021-02-03 02:41:45 +0530

    Add partition a given array haore solution program.

--- 6. Sorting/src/PartitionAGivenArray.java
+++ 6. Sorting/src/PartitionAGivenArray.java
@@ -2,7 +2,12 @@
 public class PartitionAGivenArray {
     public static void main(String[] args) {
 //        naivePartition(new int[]{10, 80, 30, 90, 40, 50, 70}, 0, 6, 6);
-        System.out.println(lomutoPartition(new int[]{10, 80, 30, 90, 40, 50, 70}, 0, 6));
+//        System.out.println(lomutoPartition(new int[]{10, 80, 30, 90, 40, 50, 70}, 0, 6));
+        int arr[] = {5, 3, 8, 4, 2, 7, 1, 10};
+        haoresPartition(arr, 0, 7);
+        for (int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
 //    Time Complexity : O(n)
@@ -45,5 +50,31 @@ public class PartitionAGivenArray {
         arr[i+1] = arr[high];
         arr[high] = temp;
         return (i+1);
+    }
+
+//    Haore's algorithm is better as compared to lomuto algorithm.
+//    Time Complexity : O(n)
+//    Auxiliary Space : O(1)
+    public static int haoresPartition(int arr[], int low, int high) {
+        int pivot = arr[low];
+        int i = low - 1;
+        int j = high + 1;
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+
+            do {
+                j--;
+            } while (arr[j] > pivot);
+
+            if (i >= j) {
+                return j;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
     }
 }
