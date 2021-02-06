de2e21a718e95c6e153f98346bb021f0c68abb8f
Suraj Singh <surajsingh220698@outlook.com>
2021-02-07 01:03:30 +0530

    Add multi-dimensional array alternative example program.

--- 7. Matrix/src/MultiDimensionalArray.java
+++ 7. Matrix/src/MultiDimensionalArray.java
@@ -1,5 +1,13 @@
 public class MultiDimensionalArray {
     public static void main(String[] args) {
+        int m=3, n=2;
+        int arr[][] = new int[m][n];
+        for (int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+        }
+        System.out.println();
         int a[][] = {{3, 4, 5},
                      {5, 1, 9}};
         for (int i=0; i<a.length; i++) {
