0d427366054879bf7f2dc5a9e4c5794b6dd86cc6
Suraj Singh <surajsingh220698@outlook.com>
2021-02-07 02:46:24 +0530

    Add the transpose of a matrix program.

---
+++ 7. Matrix/src/TransposeOfAMatrix.java
@@ -0,0 +1,27 @@
+public class TransposeOfAMatrix {
+    public static void main(String[] args) {
+        int arr[][] = {{1, 2, 3, 4},
+                        {5, 6, 7, 8},
+                        {9, 10, 11, 12},
+                        {13, 14, 15, 16}};
+        transpose(arr);
+        for (int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+//    Time Complexity : O(n*n)
+//    Auxiliary Space : O(1)
+    public static void transpose(int arr[][]) {
+        for (int i=0; i<arr.length; i++) {
+            for (int j=i+1; j<arr.length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+    }
+}
