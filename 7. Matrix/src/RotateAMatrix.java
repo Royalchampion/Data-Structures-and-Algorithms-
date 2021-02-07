840cc26159ea8dee17fb0f411b6b2044d146fe80
Suraj Singh <surajsingh220698@outlook.com>
2021-02-07 12:09:50 +0530

    Add rotate a matrix by 90 degree efficient solution program.

--- 7. Matrix/src/RotateAMatrix.java
+++ 7. Matrix/src/RotateAMatrix.java
@@ -6,7 +6,14 @@ public class RotateAMatrix {
                         {5, 6, 7, 8},
                         {9, 10, 11, 12},
                         {13, 14, 15, 16}};
-        rotateBy(arr);
+        rotateByNaive(arr);
+        for (int i=0; i<arr.length; i++) {
+            for (int j=0; j<arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+        }
+        System.out.println();
+        rotateByEfficient(arr);
         for (int i=0; i<arr.length; i++) {
             for (int j=0; j<arr.length; j++) {
                 System.out.print(arr[i][j] + " ");
@@ -15,8 +22,34 @@ public class RotateAMatrix {
     }
 
 //    Time Complexity : O(n*n)
+//    Auxiliary Space : O(1)
+    public static void rotateByEfficient(int arr[][]) {
+//        Transpose
+        for (int i=0; i<arr.length; i++) {
+            for (int j=i+1; j<arr.length; j++) {
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+//        Reverse
+        for (int i=0; i<arr.length; i++) {
+            int low = 0;
+            int high = arr.length - 1;
+            while (low<high) {
+                int temp = arr[low][i];
+                arr[low][i] = arr[high][i];
+                arr[high][i] = temp;
+                low++;
+                high--;
+            }
+        }
+    }
+
+//    Time Complexity : O(n*n)
 //    Auxiliary Space : O(n*n)
-    public static void rotateBy(int arr[][]) {
+    public static void rotateByNaive(int arr[][]) {
         int temp[][] = new int[n][n];
         for (int i=0; i<arr.length; i++) {
             for (int j=0; j<arr.length; j++) {
