15c5fb945ff3cb87db1d07ebfcbf6ad262d5871f
Suraj Singh <surajsingh220698@outlook.com>
2021-02-17 17:53:46 +0530

    Add Best time to buy and sell the stock problem program solution.

---
+++ Leetcode/Arrays/Easy/src/BestTimeToBuyAndSellTheStock.java
@@ -0,0 +1,15 @@
+public class BestTimeToBuyAndSellTheStock {
+    public static int stock(int[] array) {
+        int minValue = 0;
+        int profit = 0;
+        for (int i=0; i<array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
+            }
+            else if (array[i]-minValue > profit){
+                profit = array[i] - minValue;
+            }
+        }
+        return profit;
+    }
+}
