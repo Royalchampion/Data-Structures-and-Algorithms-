53ef84df88aece5fba3811046728ce4fd40f73bf
Suraj Singh <surajsingh220698@outlook.com>
2021-02-17 20:05:42 +0530

    Modify the whole LongestEvenOddSubarray program.

--- 4. Array/src/LongestEvenOddSubarray.java
+++ 4. Array/src/LongestEvenOddSubarray.java
@@ -7,14 +7,16 @@ public class LongestEvenOddSubarray {
 //    Auxiliary Space : O(1)
     public static int longestEvenOddSubArray(int arr[]) {
         int count = 1;
+        int result = 1;
         for (int i=0; i<arr.length-1; i++) {
             if ((arr[i]%2==0 && arr[i+1]%2!=0) || (arr[i]%2!=0 && arr[i+1]%2==0)) {
                 count++;
+                result = Math.max(result, count);
             }
-            else if (arr[i]%2==0 && arr[i+1]%2==0) {
-                continue;
+            else {
+                count = 1;
             }
         }
-        return count;
+        return result;
     }
 }
