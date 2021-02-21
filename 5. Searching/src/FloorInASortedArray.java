2ecd70f805a7b44f627b0efe51541efa6432729d
Suraj Singh <surajsingh220698@outlook.com>
2021-02-22 01:58:14 +0530

    Modify the floor in a sorted array program.

--- 5. Searching/src/FloorInASortedArray.java
+++ 5. Searching/src/FloorInASortedArray.java
@@ -3,26 +3,30 @@ import java.sql.Time;
 public class FloorInASortedArray {
 //    Time Comlexity : O(log(n))
 //    Auxiliary Space : O(1)
-    public static int floorSearch(int arr[], int low, int high, int x) {
-        if(low > high) {
-            return -1;
-        }
+    public static int floorSearch(int arr[], int x) {
+        int low = 0;
+        int high = arr.length - 1;
 
         if(x >= arr[high]) {
             return high;
         }
 
-        int mid = (low + high)/2;
+        while(low <= high) {
+            int mid = (low+high)/2;
 
-        if (arr[mid]==x) {
-            return mid;
+            if (arr[mid] == x) {
+                return mid;
+            }
+
+            else if (x < arr[mid]) {
+                high = mid - 1;
+            }
+
+            else {
+                low = mid + 1;
+            }
         }
-        if (arr[0] > 0 && arr[mid - 1]<=x && arr[mid]>x) {
-            return mid-1;
-        }
-        if (arr[mid] > x) {
-            return floorSearch(arr, low, mid - 1, x);
-        }
-        return floorSearch(arr, mid + 1, high, x);
+
+        return -1;
     }
 }
