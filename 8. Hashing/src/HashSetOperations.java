1e1f443418452f9261f3986cc4c54d2baa0f11fc
Suraj Singh <surajsingh220698@outlook.com>
2021-03-05 20:53:12 +0530

    Add hashset operations program extension.

--- 8. Hashing/src/HashSetOperations.java
+++ 8. Hashing/src/HashSetOperations.java
@@ -4,6 +4,7 @@ import java.util.Iterator;
 public class HashSetOperations {
     public static void main(String[] args) {
         HashSet<String> keywords = new HashSet<String>();
+        //  Phase 1 operation
         //  Add operation on hashset
         keywords.add("Suraj");
         keywords.add("Salman");
@@ -12,10 +13,21 @@ public class HashSetOperations {
         //  Contains operation on hashset
         System.out.println(keywords.contains("Suraj"));
         System.out.println(keywords.contains("Ronaldo"));
-        //  Iterators on hashset 
+        //  Iterators on hashset
         Iterator<String> iterator = keywords.iterator();
         while (iterator.hasNext()) {
             System.out.print(iterator.next() + " ");
         }
+        //  size operation on hashset
+        System.out.println("The size of hashset data structure is : " + keywords.size());
+        //  remove operation on hashset
+        keywords.remove("Ganesh");
+        System.out.println("The size of hashset data structure is : " + keywords.size());
+        for (String s : keywords) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        //  isEmpty operation
+        System.out.println(keywords.isEmpty());
     }
 }
