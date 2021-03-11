3f56b0ec8bdaac62ece0247c19b4ebe45006b7f1
Suraj Singh <surajsingh220698@outlook.com>
2021-03-11 21:44:48 +0530

    Add Find the longest subarray with equal no of zeros and ones efficient program solution.

--- 8. Hashing/src/LongestSubarrayWithEqualNoOfZerosAndOnes.java
+++ 8. Hashing/src/LongestSubarrayWithEqualNoOfZerosAndOnes.java
@@ -1,9 +1,14 @@
+import java.util.HashMap;
+
 public class LongestSubarrayWithEqualNoOfZerosAndOnes {
     public static void main(String[] args) {
         int arr[] = {1, 0, 1, 1, 1, 0, 0};
         System.out.println(find(arr, arr.length));
+        System.out.println(findEfficient(arr, arr.length));
     }
 
+    //  Time Complexity : O(n*n)
+    //  Auxiliary Space : O(1)
     public static int find(int[] arr, int n) {
         int result = 0;
         for (int i=0; i<n; i++) {
@@ -22,5 +27,23 @@ public class LongestSubarrayWithEqualNoOfZerosAndOnes {
             }
         }
         return result;
+    }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(n)
+    public static int findEfficient(int arr[], int n) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        int maxLength = 0;
+        for (int i=0; i<n; i++) {
+            count = count + (arr[i]==1?1:-1);
+            if (map.containsKey(count)) {
+                maxLength = Math.max(maxLength, i-map.get(count));
+            }
+            else {
+                map.put(count, i);
+            }
+        }
+        return maxLength;
     }
 }
