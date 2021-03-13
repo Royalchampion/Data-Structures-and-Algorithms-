a608e19f743bdd7b91e4e1b72a448197c4d70da4
Suraj Singh <surajsingh220698@outlook.com>
2021-03-13 13:01:45 +0530

    Add Find the longest consequence sum naive solution program.

---
+++ 8. Hashing/src/LongestConsequenceSum.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class LongestConsequenceSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 9, 3, 4, 2, 10, 13};
+        System.out.println(findLongest(arr, arr.length));
+    }
+
+    //  Time Complexity : O(nlogn)
+    //  Auxiliary Space : O(1)
+    public static int findLongest(int[] arr, int n) {
+        int count = 1;
+        int result = 0;
+        Arrays.sort(arr);
+        for (int i=1; i<arr.length; i++) {
+            if (arr[i] == arr[i-1]+1) {
+                count++;
+            }
+            else {
+                result = Math.max(result, count);
+                count = 1;
+            }
+        }
+        result = Math.max(result, count);
+        return result;
+    }
+}
