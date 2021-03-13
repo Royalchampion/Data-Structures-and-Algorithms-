569af86c97459ed472c4c397fad25f607dd397b2
Suraj Singh <surajsingh220698@outlook.com>
2021-03-13 12:45:50 +0530

    Add Find the longest common span with same sum in two binary array efficient solution program.

--- 8. Hashing/src/LongestCommonSpanWithSameSumInTwoBinaryArrays.java
+++ 8. Hashing/src/LongestCommonSpanWithSameSumInTwoBinaryArrays.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class LongestCommonSpanWithSameSumInTwoBinaryArrays {
     public static void main(String[] args) {
         int[] arr1 = {0, 1, 0, 0, 0, 0};
@@ -21,5 +23,30 @@ public class LongestCommonSpanWithSameSumInTwoBinaryArrays {
             }
         }
         return result;
+    }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(n)
+    public static int maxCommonEfficient(int[] array1, int[] array2) {
+        int temp[] = new int[array1.length];
+        for (int i=0; i<array1.length; i++) {
+            temp[i] = array1[i] - array2[i];
+        }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        int maxLength = 0;
+        for (int i=0; i<array1.length; i++) {
+            sum += temp[i];
+            if (sum==0) {
+                maxLength = i+1;
+            }
+            if (map.containsKey(sum)) {
+                maxLength = Math.max(maxLength, i - map.get(sum));
+            }
+            else {
+                map.put(sum, i);
+            }
+        }
+        return maxLength;
     }
 }
