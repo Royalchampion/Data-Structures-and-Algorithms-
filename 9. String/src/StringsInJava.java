1af4d7825279e1d6e2dc6cd532426963dbd1091c
Suraj Singh <surajsingh220698@outlook.com>
2021-03-22 18:17:51 +0530

    Add String methods program.

---
+++ 9. String/src/StringsInJava.java
@@ -0,0 +1,14 @@
+public class StringsInJava {
+    public static void main(String[] args) {
+        String str = "geeks";
+        //  Length of the string
+        System.out.println(str.length());
+
+        //  Index positioning at string
+        System.out.println(str.charAt(3));
+
+        //  String Slicing method
+        System.out.println(str.substring(2));
+        System.out.println(str.substring(2, 5));
+    }
+}
