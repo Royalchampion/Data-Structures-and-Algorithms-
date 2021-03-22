f1c55055d47f02f5caee63d2f2ffb27b5c7b2a6f
Suraj Singh <surajsingh220698@outlook.com>
2021-03-22 18:24:08 +0530

    Compare two different string comparison scenario in String methods program.

--- 9. String/src/StringsInJava.java
+++ 9. String/src/StringsInJava.java
@@ -10,5 +10,24 @@ public class StringsInJava {
         //  String Slicing method
         System.out.println(str.substring(2));
         System.out.println(str.substring(2, 5));
+
+        //  Memory for str1 is allocated while same isn't done for str2(str1 and str2 is same.)
+        String str1 = "geeks";
+        String str2 = "geeks";
+
+        if (str1==str2) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+        //  New memory is allocated for str3.
+        String str3 = new String("geeks");
+        if (str1==str3) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
     }
 }
