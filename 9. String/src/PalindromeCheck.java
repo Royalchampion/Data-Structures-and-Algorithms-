632b5fc450e46413514869ff3d1afbeed0b5f70a
Suraj Singh <surajsingh220698@outlook.com>
2021-03-22 20:43:50 +0530

    Add palindrome check efficient solution program.

--- 9. String/src/PalindromeCheck.java
+++ 9. String/src/PalindromeCheck.java
@@ -10,4 +10,19 @@ public class PalindromeCheck {
         rev.reverse();
         return str.equals(rev.toString());
     }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(1)
+    public static boolean isPalindromeEfficient(String str) {
+        int begin = 0;
+        int end = str.length()-1;
+        while (begin<end) {
+            if (str.charAt(begin)!=str.charAt(end)) {
+                return false;
+            }
+            begin++;
+            end--;
+        }
+        return true;
+    }
 }
