388e318040c1f50a5be6a4840d72819dc9cbec39
Suraj Singh <surajsingh220698@outlook.com>
2021-04-11 20:38:18 +0530

    Add check for anagram efficient solution program.

--- 9. String/src/CheckForAnagram.java
+++ 9. String/src/CheckForAnagram.java
@@ -2,11 +2,12 @@ import java.util.Arrays;
 
 public class CheckForAnagram {
     public static void main(String[] args) {
-        System.out.println(isAnagram("silent", "listan"));
+        System.out.println(isAnagramNaive("silent", "listan"));
+        System.out.println(isAnagramEfficient("silent", "listen"));
     }
 
     //  Time Complexity : O(nlogn)
-    public static boolean isAnagram(String str1, String str2) {
+    public static boolean isAnagramNaive(String str1, String str2) {
         if (str1.length()!=str2.length()) {
             return false;
         }
@@ -19,5 +20,24 @@ public class CheckForAnagram {
         Arrays.sort(b);
         str2 = new String(b);
         return str1.equals(str2);
+    }
+
+    //  Time Complexity : O(n);
+    static int character = 256;
+    public static boolean isAnagramEfficient(String str1, String str2) {
+        if (str1.length()!=str2.length()) {
+            return false;
+        }
+        int[] count = new int[character];
+        for (int i=0; i<str1.length(); i++) {
+            count[str1.charAt(i)]++;
+            count[str2.charAt(i)]--;
+        }
+        for (int i=0; i<character; i++) {
+            if (count[i]!=0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
