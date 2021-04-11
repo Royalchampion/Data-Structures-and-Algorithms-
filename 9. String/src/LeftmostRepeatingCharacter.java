e370195114ec743dc3d4dc82782f6fee9365b88d
Suraj Singh <surajsingh220698@outlook.com>
2021-04-11 21:19:39 +0530

    Add Find the leftmost repeating character good solution program.

--- 9. String/src/LeftmostRepeatingCharacter.java
+++ 9. String/src/LeftmostRepeatingCharacter.java
@@ -1,6 +1,7 @@
 public class LeftmostRepeatingCharacter {
     public static void main(String[] args) {
-        System.out.println(findLeftmostRepeatingCharacter("cabbad"));
+//        System.out.println(findLeftmostRepeatingCharacter("cabbad"));
+        System.out.println(findLeftmostRepeatingCharacterGood("geeksforgeeks"));
     }
 
     //  Time Complexity : O(n*n)
@@ -11,6 +12,20 @@ public class LeftmostRepeatingCharacter {
                 if (str.charAt(i)==str.charAt(j)) {
                     return i;
                 }
+            }
+        }
+        return -1;
+    }
+
+    static final int CHAR = 256;
+    public static int findLeftmostRepeatingCharacterGood(String str) {
+        int[] count = new int[CHAR];
+        for (int i=0; i<str.length(); i++) {
+            count[str.charAt(i)]++;
+        }
+        for (int i=0; i<str.length(); i++) {
+            if (count[str.charAt(i)]>1) {
+                return i;
             }
         }
         return -1;
