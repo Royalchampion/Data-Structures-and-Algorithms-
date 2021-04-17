facef4516425c1741c3366d7aaa795a083cfb2a8
Suraj Singh <surajsingh220698@outlook.com>
2021-04-17 23:14:48 +0530

    Add Binary String efficient solution program.

--- 9. String/src/BinaryString.java
+++ 9. String/src/BinaryString.java
@@ -3,8 +3,11 @@ public class BinaryString {
         String str = "000100100000001";
         char charArray[] = str.toCharArray();
         System.out.println(count(charArray, charArray.length));
+        System.out.println(countEfficient(charArray, charArray.length));
     }
 
+    //  Time Complexity : O(n*n)
+    //  Auxiliary Space : O(1)
     public static int count(char[] charArray, int n) {
         int count = 0;
         for (int i=0; i<n; i++) {
@@ -17,5 +20,17 @@ public class BinaryString {
             }
         }
         return count;
+    }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(1)
+    public static int countEfficient(char[] charArray, int n) {
+        int count = 0;
+        for (int i=0; i<n; i++) {
+            if (charArray[i]=='1') {
+                count++;
+            }
+        }
+        return count*(count-1)/2;
     }
 }
