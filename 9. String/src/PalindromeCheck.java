0695e9ec6d2e570cae1daeba0abbf83e791d342b
Suraj Singh <surajsingh220698@outlook.com>
2021-04-18 20:55:57 +0530

    Add another test case pass solution for palindrome check solution program.

--- 9. String/src/PalindromeCheck.java
+++ 9. String/src/PalindromeCheck.java
@@ -25,4 +25,26 @@ public class PalindromeCheck {
         }
         return true;
     }
+
+    /*
+    For "A man, a plan, a canal: Panama" input
+    int start = 0;
+        int end = s.length()-1;
+        while(start<end) {
+            if(!Character.isLetterOrDigit(s.charAt(start))) {
+                start++;
+            }
+            else if(!Character.isLetterOrDigit(s.charAt(end))) {
+                end--;
+            }
+            else {
+                if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end))) {
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+        return true;
+    */
 }
