1e23fb92a3a327d4ee94f0e2e3945ee6733e0acb
Suraj Singh <surajsingh220698@outlook.com>
2021-04-18 16:01:32 +0530

    Add find minimum indexed character efficient solution program.

--- 9. String/src/MinimumIndexedCharacter.java
+++ 9. String/src/MinimumIndexedCharacter.java
@@ -1,6 +1,9 @@
+import java.util.HashSet;
+
 public class MinimumIndexedCharacter {
     public static void main(String[] args) {
         System.out.println(findMinIndexedCharacter("onkl", "adcffaet"));
+        System.out.println(findMinIndexedCharacterEfficient("geeksforgeeks", "set"));
     }
 
     //  Time Complexity : O(n*n)
@@ -19,5 +22,28 @@ public class MinimumIndexedCharacter {
             return minIndex;
         }
         return -1;
+    }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(n)
+    public static int findMinIndexedCharacterEfficient(String str, String pat) {
+        int i;
+        int nStr = str.length();
+        int nPat = pat.length();
+        HashSet<Character> set = new HashSet<>();
+        for (i=0; i<nPat; i++) {
+            set.add(pat.charAt(i));
+        }
+        for (i=0; i<nStr; i++) {
+            if (set.contains(str.charAt(i))) {
+                break;
+            }
+        }
+        if (i<nStr) {
+            return i;
+        }
+        else {
+            return -1;
+        }
     }
 }
