d3fda7711310109c72025f86a47f25112be4e7f6
Suraj Singh <surajsingh220698@outlook.com>
2021-04-25 11:27:40 +0530

    Add Two odd occuring efficient solution program.

--- 2. Bits Manipulation/src/TwoOddNumberOccuring.java
+++ 2. Bits Manipulation/src/TwoOddNumberOccuring.java
@@ -1,6 +1,8 @@
 public class TwoOddNumberOccuring {
     public static void main(String[] args) {
         oddAppearingNaive(new int[]{4, 4, 4, 4, 5, 3, 3, 3});
+        int arr[] = new int[]{4, 4, 4, 4, 5, 3, 3, 3};
+        twoOddNumberAppearingEfficient(arr, arr.length);
     }
 
 //     Time Complexity : O(n^2)
@@ -17,5 +19,26 @@ public class TwoOddNumberOccuring {
                 System.out.println(arr[i]);
             }
         }
+    }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(1)
+    public static void twoOddNumberAppearingEfficient(int arr[], int n) {
+        int result1 = 0;
+        int result2 = 0;
+        int XOR = 0;
+        for (int i=0; i<n; i++) {
+            XOR ^= arr[i];
+        }
+        int andResult = XOR & ~(XOR-1);
+        for (int i=0; i<n; i++) {
+            if ((andResult & arr[i]) !=0) {
+                result1 = result1 ^ arr[i];
+            }
+            else {
+                result2 = result2 ^ arr[i];
+            }
+        }
+        System.out.print(result1 + " " + result2);
     }
 }
