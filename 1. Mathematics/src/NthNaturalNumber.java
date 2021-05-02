e383d9c7e27b4d42ae244a96630aca3553e39498
Suraj Singh <surajsingh220698@outlook.com>
2021-05-03 00:47:57 +0530

    Add find the nth natural numbers after removing all the numbers consisting of the digit 9.

---
+++ 1. Mathematics/src/NthNaturalNumber.java
@@ -0,0 +1,17 @@
+//  Find nth natural number after removing all the numbers consisting of the digit 9.
+public class NthNaturalNumber {
+    public static void main(String[] args) {
+        System.out.println(findNthNaturalNumber(9));
+    }
+
+    public static long findNthNaturalNumber(long number) {
+        long result = 0;
+        long pos = 1;
+        while (number>0) {
+            result += (pos*(number%9));
+            number/=9;
+            pos*=10;
+        }
+        return result;
+    }
+}
