87a67b28741da2d8dc301ae86fbc08e760e6b90b
Suraj Singh <surajsingh220698@outlook.com>
2021-05-02 09:53:57 +0530

    Add find the prime factors alternative program solution.

--- 1. Mathematics/src/PrimeFactors.java
+++ 1. Mathematics/src/PrimeFactors.java
@@ -2,6 +2,25 @@
 public class PrimeFactors {
     public static void main(String[] args) {
         primeFactors(450);
+        primeFactorsAlt(450);
+    }
+
+    //  Time Complexity : O(Math.sqrt(n))
+    //  Auxiliary Space : O(1)
+    public static void primeFactorsAlt(int number) {
+        while (number%2==0) {
+            System.out.print(2 + " ");
+            number/=2;
+        }
+        for (int i=3; i*i<=number; i+=2) {
+            while(number%i==0) {
+                System.out.print(i + " ");
+                number/=i;
+            }
+        }
+        if (number>2) {
+            System.out.print(number);
+        }
     }
 
     public static void primeFactors(int number) {
