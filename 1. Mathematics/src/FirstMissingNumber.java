8f445d8542b873bd4203b65744422c2243e16ec6
Suraj Singh <surajsingh220698@outlook.com>
2021-05-02 09:28:19 +0530

    Add find first missing number program solution.

---
+++ 1. Mathematics/src/FirstMissingNumber.java
@@ -0,0 +1,16 @@
+public class FirstMissingNumber {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 3};
+        System.out.println(findFirstMissingNumber(nums));
+    }
+
+    public static int findFirstMissingNumber(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        int numsTotal = n*(n+1)/2;
+        for(int i=0; i<n; i++) {
+            sum += nums[i];
+        }
+        return numsTotal-sum;
+    }
+}
