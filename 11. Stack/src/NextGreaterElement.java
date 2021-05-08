d9e9f9fa942f2f3538c970846f27169c82037697
Suraj Singh <surajsingh220698@outlook.com>
2021-05-08 15:20:23 +0530

    Add next greater element most efficient solution program.

--- 11. Stack/src/NextGreaterElement.java
+++ 11. Stack/src/NextGreaterElement.java
@@ -1,7 +1,26 @@
+import java.util.Stack;
+
 public class NextGreaterElement {
     public static void main(String[] args) {
         int[] arr = new int[]{5, 15, 10, 8, 6, 12, 9, 18};
         findNextGreaterElement(arr, arr.length);
+        findNextGreaterElementEfficient(arr, arr.length);
+    }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(n)
+    public static void findNextGreaterElementEfficient(int[] arr, int n) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(arr[n-1]);
+        System.out.print(-1 + " ");
+        for (int i=n-2; i>=0; i--) {
+            while (!stack.isEmpty() && stack.peek()<arr[i]) {
+                stack.pop();
+            }
+            int span = stack.isEmpty() ? -1 : stack.peek();
+            System.out.print(span + " ");
+            stack.push(arr[i]);
+        }
     }
 
     //  Time Complexity : O(n*n);
