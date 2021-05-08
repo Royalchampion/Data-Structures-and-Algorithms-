f3551e1edb83de9e974eaec1baa7530fae1f3de5
Suraj Singh <surajsingh220698@outlook.com>
2021-05-08 19:31:16 +0530

    Add find largest rectangular area efficient solution program.

--- 11. Stack/src/LargestRectangularArea.java
+++ 11. Stack/src/LargestRectangularArea.java
@@ -1,7 +1,31 @@
+import java.util.Stack;
+
 public class LargestRectangularArea {
     public static void main(String[] args) {
         int arr[] = new int[]{6, 2, 5, 4, 1, 5, 6};
         System.out.println(getMaxArea(arr, arr.length));
+        System.out.println(getMaxAreaEfficient(arr, arr.length));
+    }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(n)
+    public static int getMaxAreaEfficient(int[] arr, int n) {
+        Stack<Integer> stack = new Stack<>();
+        int res=0;
+        for (int i=0; i<n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()]>=arr[i]) {
+                int tp = stack.pop();
+                int curr = arr[tp]*(stack.isEmpty() ? i:(i-stack.peek()-1));
+                res = Math.max(res, curr);
+            }
+            stack.push(i);
+        }
+        while (!stack.isEmpty()) {
+            int tp = stack.pop();
+            int curr = arr[tp]*(stack.isEmpty() ? n : (n-stack.peek()-1));
+            res = Math.max(res, curr);
+        }
+        return res;
     }
 
     //  Time Complexity : O(n*n)
