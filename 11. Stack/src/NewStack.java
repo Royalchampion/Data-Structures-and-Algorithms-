7aa54b12e19fb5778e8b25e41889f2a2c30ab1fc
Suraj Singh <surajsingh220698@outlook.com>
2021-05-09 17:12:33 +0530

    Add implementation of stack using linked list.

---
+++ 11. Stack/src/NewStack.java
@@ -0,0 +1,52 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class NewStack {
+    Node head;
+    int sz;
+
+    NewStack() {
+        head = null;
+        sz = 0;
+    }
+
+    void push(int x) {
+        Node temp = new Node(x);
+        temp.next = temp;
+        head = temp;
+        sz++;
+    }
+
+    int pop() {
+        if (head==null) {
+            return Integer.MAX_VALUE;
+        }
+        int res = head.data;
+        Node temp = head;
+        head = temp.next;
+        sz--;
+        return res;
+    }
+
+    int peek() {
+        if (head==null) {
+            return Integer.MAX_VALUE;
+        }
+        return head.data;
+    }
+
+    int size() {
+        return sz;
+    }
+
+    boolean isEmpty() {
+        return head==null;
+    }
+}
