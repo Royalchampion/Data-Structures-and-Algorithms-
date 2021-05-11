bd766748df2ec2832a674560b1a9d4ef99f5fc48
Suraj Singh <surajsingh220698@outlook.com>
2021-05-11 17:33:03 +0530

    Add josephus problem better version.

--- 3. Recursion/src/JosephusProblem.java
+++ 3. Recursion/src/JosephusProblem.java
@@ -1,8 +1,9 @@
 public class JosephusProblem {
     public static void main(String[] args) {
-        System.out.println(josephusProblem(5, 3));
+        System.out.println(josephus(5, 3));
     }
 
+    //  This is applicable only if the number starts from 0, not one.
     //  Time Complexity : O(n)
     public static int josephusProblem(int n, int k) {
         if (n==1) {
@@ -11,5 +12,9 @@ public class JosephusProblem {
         else {
             return (josephusProblem(n-1, k) + k) % n;
         }
+    }
+
+    public static int josephus(int n, int k) {
+        return josephusProblem(n, k) + 1;
     }
 }
