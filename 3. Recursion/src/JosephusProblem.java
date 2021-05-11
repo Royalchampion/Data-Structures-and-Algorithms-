6f3c8f6a829b84afe48849b4bcc6d693c059a0f1
Suraj Singh <surajsingh220698@outlook.com>
2021-05-11 10:59:11 +0530

    Add time complexity for josephus problem.

--- 3. Recursion/src/JosephusProblem.java
+++ 3. Recursion/src/JosephusProblem.java
@@ -3,6 +3,7 @@ public class JosephusProblem {
         System.out.println(josephusProblem(5, 3));
     }
 
+    //  Time Complexity : O(n)
     public static int josephusProblem(int n, int k) {
         if (n==1) {
             return 0;
