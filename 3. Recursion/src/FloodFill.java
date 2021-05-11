ae2be3eb40406f1468a284a0c343313b4c3170ba
Suraj Singh <surajsingh220698@outlook.com>
2021-05-12 01:38:23 +0530

    Add flood fill algorithm.

---
+++ 3. Recursion/src/FloodFill.java
@@ -0,0 +1,22 @@
+public class FloodFill {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(image[sr][sc]!=newColor) {
+            floodFillUtil(image, sr, sc, image[sr][sc], newColor);
+        }
+        return image;
+    }
+
+    public void floodFillUtil(int[][] image, int sr, int sc, int prevColor, int newColor) {
+        if(sr<0 || sr>=image.length || sc<0 || sc>=image[0].length) {
+            return;
+        }
+        if(image[sr][sc]!=prevColor) {
+            return;
+        }
+        image[sr][sc] = newColor;
+        floodFillUtil(image, sr + 1, sc, prevColor, newColor);
+        floodFillUtil(image, sr - 1, sc, prevColor, newColor);
+        floodFillUtil(image, sr, sc + 1, prevColor, newColor);
+        floodFillUtil(image, sr, sc - 1, prevColor, newColor);
+    }
+}
