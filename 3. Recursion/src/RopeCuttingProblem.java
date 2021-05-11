4886e29d3aa3130c48a165df39cce3eec2012ad9
Suraj Singh <surajsingh220698@outlook.com>
2021-05-11 15:36:13 +0530

    Remove auxiliary space from rope cutting problem.

--- 3. Recursion/src/RopeCuttingProblem.java
+++ 3. Recursion/src/RopeCuttingProblem.java
@@ -4,7 +4,6 @@ public class RopeCuttingProblem {
     }
 
     //  Time Complexity : O(3**n)
-    //  Auxiliary Space : O(1)
     public static int maxPieces(int n, int a, int b, int c) {
         if (n==0) {
             return 0;
