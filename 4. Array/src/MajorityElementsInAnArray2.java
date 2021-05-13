c6501c722ae2392b5628a0fd4f968f1866833dcf
Suraj Singh <surajsingh220698@outlook.com>
2021-05-13 09:49:48 +0530

    Add majority element 2 naive solution.

--- 4. Array/src/MajorityElementsInAnArray2.java
+++ 4. Array/src/MajorityElementsInAnArray2.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MajorityElementsInAnArray2 {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 1, 1 };
         System.out.println(appearsNBy3(arr));
+        System.out.println(majorityElement(arr));
     }
 
     //  Time Complexity : O(n)
@@ -52,5 +56,27 @@ public class MajorityElementsInAnArray2 {
             return number2;
         }
         return -1;
+    }
+
+    //  Time Complexity : O(n*n)
+    //  Auxiliary Space : O(n)
+    public static List<Integer> majorityElement(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+
+        for(int i=0; i<nums.length; i++) {
+            int count = 1;
+            for(int j=i+1; j<nums.length; j++) {
+                if(nums[i]==nums[j]) {
+                    count++;
+                }
+            }
+            if(count>(nums.length/3)) {
+                if(!list.contains(nums[i])) {
+                    list.add(nums[i]);
+                }
+            }
+        }
+
+        return list;
     }
 }
