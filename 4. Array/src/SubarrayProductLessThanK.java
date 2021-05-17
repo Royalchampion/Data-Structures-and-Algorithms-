6f25fe99a127db8eafe65d849c0d4d6cac43c4d6
Suraj Singh <surajsingh220698@outlook.com>
2021-05-17 17:15:11 +0530

    Add subarray product less than k efficient solution program.

--- 4. Array/src/SubarrayProductLessThanK.java
+++ 4. Array/src/SubarrayProductLessThanK.java
@@ -22,4 +22,22 @@ public class SubarrayProductLessThanK {
         }
         return count;
     }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(1)
+    public static int numSubarrayProductLessThanKEfficient(int[] nums, int k) {
+        if (k<=1) {
+            return 0;
+        }
+        int prod = 1, ans = 0, left = 0;
+        int n = nums.length;
+        for (int right=0; right<n; right++) {
+            prod *= nums[right];
+            while (prod>=k) {
+                prod /= nums[left++];
+            }
+            ans += right-left+1;
+        }
+        return ans;
+    }
 }
