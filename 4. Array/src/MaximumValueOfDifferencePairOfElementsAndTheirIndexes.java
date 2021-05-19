6ba47d99bb501106020a271cd17c90272ce9469c
Suraj Singh <surajsingh220698@outlook.com>
2021-05-19 13:56:19 +0530

    Add maximum value of difference pair of elements and their indexes efficient program solution.

--- 4. Array/src/MaximumValueOfDifferencePairOfElementsAndTheirIndexes.java
+++ 4. Array/src/MaximumValueOfDifferencePairOfElementsAndTheirIndexes.java
@@ -15,4 +15,22 @@ public class MaximumValueOfDifferencePairOfElementsAndTheirIndexes {
         }
         return res;
     }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(1)
+    public static int maxValueEfficient(int[] arr, int N) {
+        int min1 = Integer.MAX_VALUE;
+        int max1 = Integer.MIN_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        for (int i=0; i<N; i++) {
+            max1 = Math.max(max1, arr[i]+i);
+            min1 = Math.min(min1, arr[i]+i);
+            max2 = Math.max(max2, arr[i]-i);
+            min2 = Math.min(min2, arr[i]-i);
+        }
+
+        return Math.max(max1-min1, max2-min2);
+    }
 }
