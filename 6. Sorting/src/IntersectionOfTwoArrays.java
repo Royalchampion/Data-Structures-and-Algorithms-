021dee0347228322e69510d2f90a0fb981947c8a
Suraj Singh <surajsingh220698@outlook.com>
2021-05-24 10:37:41 +0530

    Add intersection of two arrays efficient program solution.

--- 6. Sorting/src/IntersectionOfTwoArrays.java
+++ 6. Sorting/src/IntersectionOfTwoArrays.java
@@ -1,7 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class IntersectionOfTwoArrays {
     public static void main(String[] args) {
 //        findIntersectionNaive(new int[]{1, 20, 20, 20, 40, 50}, new int[]{2, 20, 20, 20});
         findIntersectionEfficient(new int[]{1, 20, 20, 20, 40, 50}, new int[]{2, 20, 20, 20});
+    }
+
+    //  This method returns an array.
+    //  Time Complexity : O(m+n)
+    //  Auxiliary Space : O(1)
+    public int[] intersection(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for(int i=0; i<m; i++) {
+            set1.add(nums1[i]);
+        }
+        for(int i=0; i<n; i++) {
+            set2.add(nums2[i]);
+        }
+
+        set1.retainAll(set2);
+
+        int[] temp = new int[set1.size()];
+        int k = 0;
+        for(int i : set1) {
+            temp[k++] = i;
+        }
+        return temp;
     }
 
 //    Time Complexity : O(m+n)
