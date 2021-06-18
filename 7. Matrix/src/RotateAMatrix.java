d775887c8d255c079a7a96295a52aa7f8f25a296
Suraj Singh <surajsingh220698@outlook.com>
2021-06-18 16:16:59 +0530

    Add rotate the matrix in clockwise direction efficient solution program solution.

--- 7. Matrix/src/RotateAMatrix.java
+++ 7. Matrix/src/RotateAMatrix.java
@@ -21,6 +21,7 @@ public class RotateAMatrix {
         }
     }
 
+//    Anti-clockwise
 //    Time Complexity : O(n*n)
 //    Auxiliary Space : O(1)
     public static void rotateByEfficient(int arr[][]) {
@@ -43,6 +44,24 @@ public class RotateAMatrix {
                 arr[high][i] = temp;
                 low++;
                 high--;
+            }
+        }
+    }
+
+    //  Clockwise
+    public static void rotateClockwise(int matrix[][]) {
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=i; j<matrix[i].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<(matrix[i].length/2); j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length-j-1];
+                matrix[i][matrix.length-j-1] = temp;
             }
         }
     }
