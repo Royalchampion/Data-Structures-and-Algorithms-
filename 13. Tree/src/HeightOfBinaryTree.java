613f453288807b717d866863786ac850f7523e5f
Suraj Singh <surajsingh220698@outlook.com>
2021-06-19 10:34:21 +0530

    Add find height of a binary tree and print nodes at k distance program solution.

--- 13. Tree/src/HeightOfBinaryTree.java
+++ 13. Tree/src/HeightOfBinaryTree.java
@@ -1,9 +1,26 @@
-import javax.swing.tree.TreeNode;
+class Node5 {
+    Node5 left;
+    Node5 right;
+    int key;
 
+    Node5(int x) {
+        key = x;
+        left = right = null;
+    }
+}
 public class HeightOfBinaryTree {
+    public static void main(String[] args) {
+        Node5 root = new Node5(10);
+        root.left = new Node5(20);
+        root.right = new Node5(30);
+        root.right.left = new Node5(25);
+        root.right.right = new Node5(35);
+        System.out.println(maxDepth(root));
+    }
+
     //  Time Complexity : O(n)
     //  Auxiliary Space : O(h)
-    public int maxDepth(TreeNode root) {
+    public static int maxDepth(Node5 root) {
         if (root==null) {
             return 0;
         }
