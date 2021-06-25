994617b1da016449c8c96ce84d27c28982ef50d5
Suraj Singh <surajsingh220698@outlook.com>
2021-06-25 23:26:58 +0530

    Add diameter of binary tree naive program solution.

---
+++ 13. Tree/src/DiameterOfBinaryTree.java
@@ -0,0 +1,44 @@
+import javax.swing.tree.TreeNode;
+
+class Node18 {
+    int key;
+    Node18 left;
+    Node18 right;
+
+    Node18(int x) {
+        key = x;
+        left = right = null;
+    }
+}
+
+public class DiameterOfBinaryTree {
+    public static void main(String[] args) {
+        Node18 root = new Node18(1);
+        root.left = new Node18(2);
+        root.right = new Node18(3);
+        root.left.left = new Node18(4);
+        root.left.right = new Node18(5);
+        System.out.println(diameterOfBinaryTree(root));
+    }
+
+    //  Time Complexity : O(n*n)
+    //  Auxiliary Space : O(h)
+    public static int diameterOfBinaryTree(Node18 root) {
+        if(root==null) {
+            return 0;
+        }
+        int d1 = 1 + height(root.left) + height(root.right);
+        int d2 = diameterOfBinaryTree(root.left);
+        int d3 = diameterOfBinaryTree(root.right);
+        return Math.max(d1+1, Math.max(d2, d3));
+    }
+
+    public static int height(Node18 root) {
+        if(root==null) {
+            return -1;
+        }
+        else {
+            return (Math.max(height(root.left), height(root.right))+1);
+        }
+    }
+}
