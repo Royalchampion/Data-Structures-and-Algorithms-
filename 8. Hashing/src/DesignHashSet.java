3bff6dbcfd49aa67af98b7e8f76fddb745f250a5
Suraj Singh <surajsingh220698@outlook.com>
2021-06-28 17:16:21 +0530

    Add Design Hashset efficient program solution.

--- 8. Hashing/src/DesignHashSet.java
+++ 8. Hashing/src/DesignHashSet.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 public class DesignHashSet {
     //  Time Complexity : O(1)
@@ -22,4 +25,49 @@ public class DesignHashSet {
     public boolean contains(int key) {
         return arr[key];
     }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    //  Time Complexity : O(1)
+    //  Auxiliary Space : O(1)
+    /*
+    private static final int MAX_VALUE = 1000000;
+    private static final int ARRAY_SIZE = 100;
+    List<List<Integer>> list;
+
+    public MyHashSet() {
+        list = new ArrayList<>(ARRAY_SIZE);
+        for (int i=0; i<ARRAY_SIZE; i++) {
+            list.add(null);
+        }
+    }
+
+    public void add(int key) {
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = list.get(index);
+        if(childList==null) {
+            List<Integer> newList = new LinkedList<>();
+            newList.add(key);
+            list.set(index, newList);
+        }
+        else {
+            if(!childList.contains(key)) {
+                childList.add(key);
+            }
+        }
+    }
+
+    public void remove(int key) {
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = list.get(index);
+        if(childList!=null) {
+            childList.remove(Integer.valueOf(key));
+        }
+    }
+
+    public boolean contains(int key) {
+        int index = key % ARRAY_SIZE;
+        List<Integer> childList = list.get(index);
+        return childList != null && childList.contains(key);
+    }
+    */
 }
