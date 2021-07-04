f6736d67f26dde3a1f10604f54265cb312252c9a
Suraj Singh <surajsingh220698@outlook.com>
2021-07-04 12:33:39 +0530

    Add fundamental operation being performed on binary search tree for integers.

--- N. Binary Search Tree/src/TreeSetExample.java
+++ N. Binary Search Tree/src/TreeSetExample.java
@@ -2,25 +2,47 @@ import java.util.Iterator;
 import java.util.TreeSet;
 
 public class TreeSetExample {
-    public static void main(String[] args) {\
+    public static void main(String[] args) {
         //  TreeSet
-        TreeSet<String> set = new TreeSet<>();
+        TreeSet<String> stringSet = new TreeSet<>();
 
         //  Add operation
-        set.add("geeks");
-        set.add("for");
-        set.add("students");
+        stringSet.add("geeks");
+        stringSet.add("for");
+        stringSet.add("students");
 
-        //  Print the sets
-        System.out.println(set);
+        //  Print the stringSets
+        System.out.println(stringSet);
 
         //  Boolean operation
-        System.out.println(set.contains("geeks"));
+        System.out.println(stringSet.contains("geeks"));
 
         //  Iteration
-        Iterator<String> iterator = set.iterator();
+        Iterator<String> iterator = stringSet.iterator();
         while (iterator.hasNext()) {
             System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+
+        //  TreeSet for integers
+        TreeSet<Integer> integerSet = new TreeSet<>();
+
+        //  Add operation
+        integerSet.add(2);
+        integerSet.add(4);
+        integerSet.add(1);
+        integerSet.add(6);
+
+        //  Print
+        System.out.println(integerSet);
+
+        //  Remove operation
+        integerSet.remove(4);
+
+        //  Iteration
+        Iterator<Integer> iteratorInteger = integerSet.iterator();
+        while (iteratorInteger.hasNext()) {
+            System.out.print(iteratorInteger.next() + " ");
         }
     }
 }
