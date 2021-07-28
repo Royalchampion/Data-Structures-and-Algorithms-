6eb644549ad1188888c514509e85f595af45034f
Suraj Singh <surajsingh220698@outlook.com>
2021-07-28 16:10:44 +0530

    Add preorder traversal of tree iteratively.

--- M. Tree/src/PreOrderTraversal.java
+++ M. Tree/src/PreOrderTraversal.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
 class Node3 {
     int key;
     Node3 left;
@@ -16,16 +19,41 @@ public class PreOrderTraversal {
         root.right = new Node2(30);
         root.right.left = new Node2(25);
         root.right.right = new Node2(35);
-        preOrder(root);
+        preOrderIterative(root);
+        System.out.println();
+        preOrderRecursive(root);
     }
 
     //  Time Complexity : O(n)
     //  Auxiliary Space : O(h)
-    public static void preOrder(Node2 root) {
+    public static void preOrderIterative(Node2 root) {
+        if (root==null) {
+            return;
+        }
+        Stack<Node2> stack = new Stack<Node2>();
+        stack.add(root);
+        Node2 curr = root;
+        while (curr!=null || !stack.isEmpty()) {
+            while (curr!=null) {
+                System.out.print(curr.key + " ");
+                if (curr.right!=null) {
+                    stack.add(curr.right);
+                }
+                curr = curr.left;
+            }
+            if (!stack.isEmpty()) {
+                curr = stack.pop();
+            }
+        }
+    }
+
+    //  Time Complexity : O(n)
+    //  Auxiliary Space : O(h)
+    public static void preOrderRecursive(Node2 root) {
         if (root!=null) {
             System.out.print(root.key + " ");
-            preOrder(root.left);
-            preOrder(root.right);
+            preOrderRecursive(root.left);
+            preOrderRecursive(root.right);
         }
     }
 }
