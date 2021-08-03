474d45e4a2d3ece4148626271708b8d7daf0e2fe
Suraj Singh <surajsingh220698@outlook.com>
2021-08-03 15:51:40 +0530

    Add minimum time to burn a binary tree from a leaf node program solution.

---
+++ M. Tree/src/MinimumTimeToBurnABinaryTreeFromLeafNode.java
@@ -0,0 +1,50 @@
+import java.util.Map;
+
+class Node31 {
+    int val;
+    Node31 left;
+    Node31 right;
+
+    Node31(int val) {
+        val = val;
+        left = right = null;
+    }
+}
+
+class Data {
+    int val;
+
+    public Data(int distance) {
+        val = distance;
+    }
+}
+
+public class MinimumTimeToBurnABinaryTreeFromLeafNode {
+    int res = 0;
+    public int minimumPoint(Node31 root, Data data, int target) {
+        if (root==null) return 0;
+
+        if (root.val==target) {
+            data.val = 0;
+            return 1;
+        }
+
+        Data leftDistance = new Data(-1);
+        int leftHeight = minimumPoint(root.left, leftDistance, target);
+
+        Data rightDistance = new Data(-1);
+        int rightHeight = minimumPoint(root.right, rightDistance, target);
+
+        if (leftDistance.val!=-1) {
+            data.val = leftDistance.val + 1;
+            res = Math.max(res, data.val+leftHeight);
+        }
+
+        else if (rightDistance.val!=-1) {
+            data.val = rightDistance.val + 1;
+            res = Math.max(res, data.val+rightHeight);
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
